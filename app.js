a56ea7b11f15abc1026b924ebde3fb3bfd96f5e1
Rungta <rungta.oshin@gmail.com>
2018-01-02 16:10:34 +0530

    NameCharCalculator

--- app.js
+++ app.js
@@ -3,8 +3,26 @@
 
 angular.module('myFirstApp',[])
 
-.controller('MyFirstController',function()
+.controller('MyFirstController',function($scope)
 {
+  $scope.name="";
+  $scope.totalvalue=0;
+$scope.displayNumeric=function()
+{
+  var totalNamevalue=calculateNumericString($scope.name);
+  $scope.totalvalue=totalNamevalue;
+};
+function calculateNumericString(string)
+{
+  var totalStringvalue=0;
+  for(var i=0;i<string.length;i++)
+  {
+  totalStringvalue+=string.charCodeAt(i);
+}
+return totalStringvalue;
+
+};
+
 
 });
 
